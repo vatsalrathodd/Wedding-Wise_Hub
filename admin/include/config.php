ca55b3a93f26f69f1ac03118a334e709236ba611
vatsalrathodd <vatsalrathodd@gmail.com>
2024-02-28 02:10:37 +0530

    first commit

---
+++ admin/include/config.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('DB_CONFIG_INCLUDED')) {
+    define('DB_CONFIG_INCLUDED', true);
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'dbwedding');
+}
+?>
